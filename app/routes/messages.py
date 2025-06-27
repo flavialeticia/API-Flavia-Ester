8ab35855116a864c43454d44e141252ff9013131
Flavia <133375960+flavialeticia@users.noreply.github.com>
2025-06-27 11:54:25 +0000

    resolvendo problema do messages

--- app/routes/messages.py
+++ app/routes/messages.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify # Adicionar jsonify
 from ..models.message import Message
 from .. import db
 from ..schemas.message_schema import MessageSchema
@@ -10,23 +10,26 @@ messages_schema = MessageSchema(many=True)
 @messages_bp.route('/', methods=['GET'])
 def get_messages():
     messages = Message.query.all()
-    return messages_schema.jsonify(messages), 200
+    # Modificar para usar jsonify do Flask com dump do schema
+    return jsonify(messages_schema.dump(messages)), 200
 
 @messages_bp.route('/<int:message_id>', methods=['GET'])
 def get_message(message_id):
     message = Message.query.get_or_404(message_id)
-    return message_schema.jsonify(message), 200
+    # Modificar para usar jsonify do Flask com dump do schema
+    return jsonify(message_schema.dump(message)), 200
 
 @messages_bp.route('/', methods=['POST'])
 def create_message():
     data = request.get_json()
     message = Message(
         content=data['content'],
-        user_id=1  # Usuário padrão
+        user_id=1
     )
     db.session.add(message)
     db.session.commit()
-    return message_schema.jsonify(message), 201
+    # Modificar para usar jsonify do Flask com dump do schema
+    return jsonify(message_schema.dump(message)), 201
 
 @messages_bp.route('/<int:message_id>', methods=['PUT'])
 def update_message(message_id):
@@ -35,10 +38,10 @@ def update_message(message_id):
 
     if 'content' in data:
         message.content = data['content']
-    # Não permitimos alterar o user_id após a criação
     
     db.session.commit()
-    return message_schema.jsonify(message), 200
+    # Modificar para usar jsonify do Flask com dump do schema
+    return jsonify(message_schema.dump(message)), 200
 
 @messages_bp.route('/<int:message_id>', methods=['DELETE'])
 def delete_message(message_id):
